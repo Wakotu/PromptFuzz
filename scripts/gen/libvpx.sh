b426baddac1ed3cbf8581f5012ae85c1527814ea
yq4ng <yq4ng@foxmail.com>
2026-03-23 22:58:55 +0800

    Adjust generation model

--- scripts/gen/libvpx.sh
+++ scripts/gen/libvpx.sh
@@ -3,4 +3,4 @@ export OPENAI_ENDPOINT="https://api.gpt.ge/v1"
 export OPENAI_MODEL_NAME="deepseek-v3.2"
 export OPENAI_API_KEY="sk-YKl9eTYP6Kv3WFYPE804A3Ee87Bd42588cCdEe036295146a"
 
-time cargo run --bin fuzzer -- libvpx
+cargo run --bin fuzzer -- libvpx
