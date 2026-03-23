004fb75eafd4b32d9c541f88ebb303b8c1327b42
yq4ng <yq4ng@foxmail.com>
2026-03-23 08:52:36 +0000

    Script: add time wrapper

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-libs=( "cjson" "libaom" "libpcap" "libpng" "libvpx" "lcms" "zlib" "sqlite3")
+# libs=( "cjson" "libaom" "libpcap" "libpng" "libvpx" "lcms" "zlib" "sqlite3")
+libs=( "libvpx" "lcms" "cjson" "libaom" "libpcap" "libpng" "zlib" "sqlite3")
 
 for lib in "${libs[@]}"
 do
